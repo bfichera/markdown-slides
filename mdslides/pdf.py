b29772196caaae7775f74fa5ddde34a4f7c9f8ee
Bryan Fichera <bfichera@mit.edu>
2023-08-05 12:55:35 -0400

    Revert previous and add --gpu-disable

--- mdslides/pdf.py
+++ mdslides/pdf.py
@@ -12,7 +12,7 @@ def pdf_chromium_export(index_html_path: Path, output_pdf_path: Path):
     command = [
         'chromium',
         '--headless',
-        '--use-gl=desktop',
+        '--gpu-disable',
         '--print-to-pdf={}'.format(output_pdf_path),
         index_html_path.resolve().as_uri()+'?print-pdf',
     ]
